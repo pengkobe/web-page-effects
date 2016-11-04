b9b04a8620fbaa53fc007d81bf16143cc9b3bf8b
pengkobe <526337223@qq.com>
2016-11-04 16:46:15 +0800

    add animation for dashboard

--- canvas/dashboard.js
+++ canvas/dashboard.js
@@ -4,14 +4,29 @@
 
 (function () {
     var canvas = document.getElementById("showdashboard");
-    var value = 0.75;
-    var radius = 40;
-    var parts = 50;
-    if (canvas.getContext) {
-        ctx = canvas.getContext("2d");
-        setValueText(ctx, canvas, value);
-        drawCircle(ctx, parts, radius);
-        drawDataPointer(ctx, value, radius);
+    var hasRendered = false;
+    window.InitDash = function (params) {
+        if (canvas.getContext) {
+            ctx = canvas.getContext("2d");
+            // move axies to center
+            ctx.translate(canvas.width / 2, canvas.height / 2);
+            // Use the identity matrix while clearing the canvas
+            if (!hasRendered) {
+                setValueText(ctx, canvas, params.value);
+                drawCircle(ctx, params.parts, params.radius);
+                drawDataPointer(ctx, params.value, params.radius);
+                hasRendered = true;
+            } else {
+                // Use the identity matrix while clearing the canvas
+                ctx.setTransform(1, 0, 0, 1, 0, 0);
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                // Restore the transform
+                ctx.translate(canvas.width / 2, canvas.height / 2);
+                setValueText(ctx, canvas, params.value);
+                drawCircle(ctx, params.parts, params.radius);
+                drawDataPointer(ctx, params.value, params.radius);
+            }
+        }
     }
 
     /**
@@ -21,16 +36,15 @@
     * @param {Number} value [当前值]
     */
     function setValueText(ctx, canvas, value) {
-        // move axies to center
-        ctx.translate(canvas.width / 2, canvas.height / 2);
-
+        ctx.save();
         if (value <= 0.5) {
             ctx.fillStyle = 'rgb(' + parseInt(510 * value) + ',255,0)';
         } else {
             ctx.fillStyle = 'rgb(255,' + parseInt(2 * (1 - value) * 255) + ',0)';
         }
-        ctx.textAlign="center";
+        ctx.textAlign = "center";
         ctx.fillText(value * 100, 0, radius - 11);
+        ctx.restore();
     }
 
     /**
@@ -40,15 +54,18 @@
     * @param {Number} radius [半径]
     */
     function drawCircle(ctx, parts, radius) {
+        ctx.save();
         var beginAngle = Math.PI / 6;
         var endAngle = Math.PI * 11 / 6;
 
         ctx.rotate(Math.PI / 2);
         ctx.strokeStyle = "black";
         ctx.lineWidth = 1;
+        ctx.beginPath();
         // x,y,radius,sAngle,eAngle
         ctx.arc(0, 0, radius, beginAngle, endAngle);
         ctx.stroke();
+        ctx.closePath();
         ctx.rotate(beginAngle - Math.PI);
         var divides = 255 / (parts / 2);
         for (var i = 0; i <= parts; i++) {
@@ -65,7 +82,8 @@
             ctx.closePath();
             ctx.rotate((endAngle - beginAngle) / 51);
         }
-        ctx.save();
+
+        ctx.restore();
     }
 
     /**
@@ -75,10 +93,11 @@
     * @param {Number} radius [半径]
     */
     function drawDataPointer(ctx, value, radius) {
+        ctx.save();
         var beginAngle = Math.PI / 6;
         var endAngle = Math.PI * 11 / 6;
 
-        ctx.rotate(2 * beginAngle + value * (endAngle - beginAngle));
+        ctx.rotate(-Math.PI / 2 + beginAngle + value * (endAngle - beginAngle));
         ctx.beginPath();
         ctx.arc(-radius + 11, 0, 3, 0, 2 * Math.PI, true);
         if (value <= 0.5) {
@@ -88,8 +107,18 @@
         }
         ctx.fill();
         ctx.closePath();
-        ctx.save();
+        ctx.restore();
     }
 })();
 
 
+var value = 0.75;
+var radius = 40;
+var parts = 50;
+setInterval(function () {
+    value += 0.1;
+    if (value > 1) {
+        value = 0.1;
+    }
+    InitDash({ value: value, radius: radius, parts: parts });
+}, 1000)
